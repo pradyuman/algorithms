20758a0beca756c292a2c2bed19a7f358ab9ad02
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-02-08 23:24:19 -0500

    Factorial.c completed

---
+++ factorial.c
@@ -0,0 +1,24 @@
+//This program calculates the factorial of a number.
+#include <stdio.h>
+#include <stdlib.h>
+
+//Function declaration
+unsigned factorial(unsigned num);
+
+
+//Main
+int main(int argc, char * argv[]){
+    int ans, sum;
+
+    ans = factorial(atoi(argv[1]));
+
+    printf("The factorial of %d is %d.\n", atoi(argv[1]), ans);
+}
+
+//Function definition
+unsigned factorial(unsigned num){
+    if (num == 1){
+        return 1;
+    }
+    return num * factorial(num - 1);
+}

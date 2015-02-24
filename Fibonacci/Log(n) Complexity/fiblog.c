4865f5dd142eee6c8f63278924380f1163ffbf0c
Scyphi <pradyumanvig@hotmail.com>
2015-02-24 12:50:02 -0500

    Added comments

--- Fibonacci/Log(n) Complexity/fiblog.c
+++ Fibonacci/Log(n) Complexity/fiblog.c
@@ -4,14 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-
+   //Error checking
    if (argc != 2) {
       return EXIT_FAILURE;
    }
+   
    int n = atoi(argv[1]);
    if (n < 0) {
       return EXIT_FAILURE;
    }
+   
+   //Call Fibonacci function
    unsigned long ans = Fibonacci(n);
    
    printf("The Fibonacci number is: %lu\n", ans);

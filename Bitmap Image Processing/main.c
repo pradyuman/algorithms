ba0a3b17ba8d4c48d4a87f3b3cac0f8eebbbc43f
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-04-09 14:53:53 -0400

    modified main to take input for conversion

--- Bitmap Image Processing/main.c
+++ Bitmap Image Processing/main.c
@@ -92,6 +92,51 @@ int main(int argc, char** argv){
       Free_BMP_Image(image);
       Free_BMP_Image(outputImage);
    }
+   else if (argc >= 4 && strcmp(argv[1],"-c") == 0){
+      //Opening input file
+      FILE* input = fopen(argv[2],"rb");
+      if (input == NULL) {
+         return EXIT_FAILURE;
+      }
+      
+      //Reading file
+      BMP_Image *image = Read_BMP_Image(input);
+      if (image == NULL) {
+         fclose(input);
+         return EXIT_FAILURE;
+      }
+      /*
+       *Need to edit this part
+       
+      BMP_Image *outputImage = Left_Half_BMP_Image(image);
+      if (outputImage == NULL) {
+         fclose(input);
+         Free_BMP_Image(image);
+         return EXIT_FAILURE;
+      }*/
+      
+      //Opening output file
+      FILE* output = fopen(argv[3],"wb");
+      if (output == NULL) {
+         fclose(input);
+         return EXIT_FAILURE;
+      }
+      
+      //Writing to output file
+      int valid = Write_BMP_Image(output, outputImage);
+      if (valid == FALSE) {
+         fclose(input);
+         fclose(output);
+         Free_BMP_Image(image);
+         Free_BMP_Image(outputImage);
+      }
+      
+      
+      fclose(input);
+      fclose(output);
+      Free_BMP_Image(image);
+      Free_BMP_Image(outputImage);
+   }
    else {
       return(EXIT_FAILURE);
    }

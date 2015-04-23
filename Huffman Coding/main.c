2fe29c2adc364bcc3f0275e5e7473658bbffd9c0
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-04-22 21:08:05 -0400

    fixed some valgrind errors

--- Huffman Coding/main.c
+++ Huffman Coding/main.c
@@ -2,16 +2,15 @@
 
 int main(int argc, char **argv) {
    //Checking to make sure that the correct number of arguments is given (at least 3)
-   
    if (argc < 3) {
-      fprintf(stderr, "main.c:8 | ERROR 01: Incorrect number of input arguments");
+      fprintf(stderr, "main.c:5 | ERROR 01: Incorrect number of input arguments");
       return EXIT_FAILURE;
    }
    
    //Opening input file
    FILE *input = fopen(argv[1], "r");
    if (input == NULL) {
-      fprintf(stderr, "main.c:15 | ERROR 02: Input file could not be opened");
+      fprintf(stderr, "main.c:11 | ERROR 02: Input file could not be opened");
       return EXIT_FAILURE;
    }
    
@@ -21,21 +20,29 @@ int main(int argc, char **argv) {
    fclose(input);
    
    if (huffman == NULL) {
-      fprintf(stderr, "main.c:25 | ERROR 04: Huffman tree could not be created");
+      fprintf(stderr, "main.c:18 | ERROR 04: Huffman tree could not be created");
       return EXIT_FAILURE;
    }
    
    //Opening output file
    FILE *output = fopen(argv[2], "w");
    if (output == NULL) {
-      fprintf(stderr, "main.c:31 | ERROR 05: Output file could not be opened or created");
+      fprintf(stderr, "main.c:28 | ERROR 05: Output file could not be opened or created");
       return EXIT_FAILURE;
    }
    
    //Save the huffman tree into a file
    char *code = (char *)malloc(sizeof(char) * 50);
    postOrderPrint(huffman, output, code);
-   
+   if (output == NULL) {
+      free(code);
+      destructTree(huffman);
+      fclose(output);
+      fprintf(stderr, "main.c:36 | ERROR 05: Output file could not be opened or created");
+      return EXIT_FAILURE;
+   }
+   destructTree(huffman);
+   free(code);
    //Output file is not needed after printing
    fclose(output);
    

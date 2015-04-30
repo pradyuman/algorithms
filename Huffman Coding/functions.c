7e094bf40fd6c3eb3732de846224bdeccff9bb75
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-04-29 23:42:51 -0400

    debugging huffman coding

--- Huffman Coding/functions.c
+++ Huffman Coding/functions.c
@@ -8,11 +8,87 @@ treeNode *constructHuffmanTree(FILE *input, int version) {
    stack.tree = (treeNode *)0;
    
    int token;
-   int pos;
-   int mask;
-   int readChar;
+   int pos = -1;
+   int readChar = 0;
    
-   if (version==BIT) pos = -1; //version = BIT means that it is bit-based header
+   if (version==BIT) {  //version = BIT means that it is bit-based header
+     while (1) {
+        if (pos < 0) {
+           readChar = fgetc(input);
+           if (readChar == EOF) {
+              token = -1;
+              break;
+           }
+           pos = 7;
+        }
+         int mask = 1 << pos--;
+         token = (readChar & mask) == 0 ? '0' : '1';
+         
+      if (token == '0') {
+         if (stackSize(&stack) <= 1)
+            break;
+         
+         listNode *rightNode = pop(&stack);
+         listNode *leftNode = pop(&stack);
+         
+         treeNode *rightTree = rightNode->tree;
+         treeNode *leftTree = leftNode->tree;
+         
+         treeNode *tree = constructTree(ASCII_COUNT, leftTree, rightTree);
+         
+         if (tree == NULL) {
+            fprintf(stderr, "functions.c:26 | ERROR 06: Tree could not be constructed");
+            destructNode(leftNode);
+            destructNode(rightNode);
+            token = EXCEPTION;
+            break;
+         }
+         
+         rightNode->tree = tree;
+         rightNode->next = NULL;
+         push(&stack, rightNode);
+         free(leftNode);
+      }
+      else if (token == '1') {
+         if (pos < 0) {
+            token = fgetc(input);
+            if (token == EOF)
+               break;
+         } else {
+            mask = 0xFF >> (7 - pos);
+            token = (readChar & mask) << (7-pos);
+            readChar = fgetc(input);
+            if (readChar == EOF) {
+               token = EOF;
+               break;
+            }
+            token |= readChar >> (pos + 1); 
+         }   
+         
+         if (token == EOF)
+            break;
+         
+         treeNode *tree = constructTree(token, NULL, NULL);
+         if (tree == NULL) {
+            fprintf(stderr, "functions.c:45 | ERROR 06: Tree could not be constructed");
+            token = EXCEPTION;
+            break;
+         }
+         
+         listNode *stackNode = constructNode(tree);
+         if (stackNode == NULL) {
+            fprintf(stderr, "functions.c:26 | ERROR 07: List node could not be constructed");
+            destructTree(tree);
+            token = EXCEPTION;
+            break;
+         }
+         push(&stack, stackNode);
+      }
+      else
+         break;
+   } 
+      
+   } else {
    
    while ((token = fgetc(input)) != EOF) {
       if (version == BIT) {
@@ -54,7 +130,7 @@ treeNode *constructHuffmanTree(FILE *input, int version) {
                if (token == EOF)
                   break;
             } else {
-               mask = 0xFF >> (7 - pos);
+               int mask = 0xFF >> (7 - pos);
                token = (readChar & mask) << (7-pos);
                readChar = fgetc(input);
                if(readChar == EOF) {
@@ -89,6 +165,7 @@ treeNode *constructHuffmanTree(FILE *input, int version) {
       }
       else
          break;
+   }
    }
    
    if (token == '0' && stackSize(&stack) == 1) {
